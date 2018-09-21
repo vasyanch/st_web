7ac4260557acffd2751c9f6fbfc9d4e59ff07130
box <box@stepik.ru>
2018-09-21 11:40:13 +0000

    nginx_1.8

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 # sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
