08f2e51109fec66dc92617553d736ee4fcbaad8f
Vasily Mikhaylov <vasyanch852@gmail.com>
2018-09-24 00:16:54 +0300

    ask

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
 
     url(r'^', test),
     url(r'^login/', test),
-    url(r'^signup/', test,
+    url(r'^signup/', test),
     url(r'^question/123/', test),
     url(r'^ask/', test),
     url(r'^popular/', test),
