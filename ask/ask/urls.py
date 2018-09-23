6a333990e554f0ed029c8e16ec72f857ed5bf86c
Vasily Mikhaylov <vasyanch852@gmail.com>
2018-09-23 22:58:22 +0300

    ask

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
 from qa import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
     url(r'^', views.test),
     url(r'^login/', views.test),
     url(r'^signup/', views.test,
