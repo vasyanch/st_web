85bd83ffc8a4b64c2e9c88927f98fe6e484c53ab
Vasily Mikhaylov <vasyanch852@gmail.com>
2018-09-23 23:20:42 +0300

    ask

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/gunicorn.config /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
  
 #sudo gunicorn -b 0.0.0.0:8080 hello:ws &
