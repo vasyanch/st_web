8fb3998752dfe7b3e59d284f5ff2aba3fffb306b
taga <taga>
2018-09-26 17:29:22 +0300

    2.3db

--- ask/initdb.sh
+++ ask/initdb.sh
@@ -1,2 +1,2 @@
 mysql -uroot -e "create database ask";
-mysql -uroot -e "create user 'root'@'localhost';
+mysql -uroot -e "create user "root"@"localhost";
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_lenght=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField(default=0)
+    author = models.CharField(max_lenght=255)
+    likes = models.
